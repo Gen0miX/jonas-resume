b59fc78b216828af271705c9a4aa1bc8747ce00d
Gen0miX <J0naas_14@hotmail.com>
2024-10-26 23:10:43 +0200

    About me responsive ok

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -4,47 +4,57 @@ import { AiFillGithub, AiFillLinkedin, AiFillMail } from "react-icons/ai";
 
 export default function AboutMe() {
   return (
-    <section id="about-me" className="flex m-60 scroll-mt-32">
-      <Image
-        src={profilePic}
-        alt={"Profile picture"}
-        width={400}
-        height={600}
-        className="mr-20"
-      />
-      <div className="flex flex-col justify-center">
-        <h1 className="font-heading text-center text-6xl mb-10">ABOUT ME</h1>
-        <p className="font-sans font-medium text-justify text-xl">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
-          suscipit odit recusandae facilis consequuntur asperiores minus, earum
-          repudiandae est iste officiis. Tenetur rerum maiores nisi corporis?
-          Repellat mollitia vero quia obcaecati quisquam rem ex blanditiis iure
-          eum aspernatur nihil voluptatum officia optio consequuntur a, quaerat
-          inventore provident itaque eligendi nemo in ea consectetur. Possimus
-          tempore modi esse quam deleniti laborum. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Dolor architecto quae mollitia
-          similique, cupiditate eos recusandae maxime veniam voluptatibus
-          impedit. Harum temporibus necessitatibus ab, accusamus nulla est
-          provident placeat, neque corrupti debitis impedit vero alias quo
-          officiis aliquam quasi doloremque natus, reiciendis consequuntur
-          similique sint atque sequi. Dolore temporibus assumenda omnis nobis
-          ipsam, modi maxime ratione at quo vero suscipit veritatis ducimus
-          aspernatur soluta cumque deleniti nisi enim neque hic voluptates
-          obcaecati dolores natus commodi a? Veritatis fuga odit ipsa, libero
-          accusantium assumenda voluptates quae adipisci suscipit nesciunt
-          accusamus unde facere ullam. Eos corrupti, velit voluptas repudiandae
-          accusantium libero dolores.
-        </p>
-        <div className="flex justify-end mt-10">
-          <a className="text-3xl mx-1" href="">
-            <AiFillLinkedin></AiFillLinkedin>
-          </a>
-          <a className="text-3xl mx-1" href="">
-            <AiFillGithub></AiFillGithub>
-          </a>
-          <a className="text-3xl mx-1" href="">
-            <AiFillMail></AiFillMail>
-          </a>
+    <section
+      id="about-me"
+      className="flex flex-col mt-40 scroll-mt-28 xl:mx-32 2xl:m-60 lg:items-start"
+    >
+      <h1 className="font-heading text-6xl mb-5 ml-10 hidden lg:block">
+        ABOUT ME
+      </h1>
+      <div className="flex flex-col items-center lg:flex-row">
+        <Image
+          src={profilePic}
+          alt={"Profile picture"}
+          width={400}
+          height={600}
+          className="mb-5 h-[60vh] lg:ml-10"
+        />
+        <div className="flex flex-col mx-10 justify-center">
+          <h1 className="font-heading text-6xl my-5 text-left lg:hidden">
+            ABOUT ME
+          </h1>
+          <p className="font-sans font-medium text-justify text-xl">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo
+            suscipit odit recusandae facilis consequuntur asperiores minus,
+            earum repudiandae est iste officiis. Tenetur rerum maiores nisi
+            corporis? Repellat mollitia vero quia obcaecati quisquam rem ex
+            blanditiis iure eum aspernatur nihil voluptatum officia optio
+            consequuntur a, quaerat inventore provident itaque eligendi nemo in
+            ea consectetur. Possimus tempore modi esse quam deleniti laborum.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            architecto quae mollitia similique, cupiditate eos recusandae maxime
+            veniam voluptatibus impedit. Harum temporibus necessitatibus ab,
+            accusamus nulla est provident placeat, neque corrupti debitis
+            impedit vero alias quo officiis aliquam quasi doloremque natus,
+            reiciendis consequuntur similique sint atque sequi. Dolore
+            temporibus assumenda omnis nobis ipsam, modi maxime ratione at quo
+            vero suscipit veritatis ducimus aspernatur soluta cumque deleniti
+            nisi enim neque hic voluptates obcaecati dolores natus commodi a?
+            Veritatis fuga odit ipsa, libero accusantium assumenda voluptates
+            quae adipisci suscipit nesciunt accusamus unde facere ullam. Eos
+            corrupti, velit voluptas repudiandae accusantium libero dolores.
+          </p>
+          <div className="flex justify-end mt-10">
+            <a className="text-3xl mx-1" href="">
+              <AiFillLinkedin></AiFillLinkedin>
+            </a>
+            <a className="text-3xl mx-1" href="">
+              <AiFillGithub></AiFillGithub>
+            </a>
+            <a className="text-3xl mx-1" href="">
+              <AiFillMail></AiFillMail>
+            </a>
+          </div>
         </div>
       </div>
     </section>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 function NavItem({ children, href, ...props }: Props) {
   return (
-    <li className="antialiased font-heading font-bold text-[12vw] hover:-skew-x-6 hover:scale-105 hover:scale-y-125 transition-transform duration-300 ease-in xl:font-normal md:text-[10.5vw] lg:text-[8vw]">
+    <li className="antialiased font-heading font-bold text-[12vw] hover:-skew-x-6 hover:scale-105 hover:scale-y-125 transition-transform duration-300 ease-in xl:font-normal md:text-[9vw] lg:text-[8vw]">
       <Link href={href}>{children}</Link>
     </li>
   );
@@ -47,7 +47,7 @@ export default function Hero() {
         )}
       </div>
       <div className="flex flex-col mx-5 grow max-h-full justify-end lg:max-w-full lg:flex-row lg:mx-0 lg:justify-evenly">
-        <div className="flex flex-col mt-10 max-w-sm lg:mt-0 lg:mx-5 lg:justify-end lg:flex-1 lg:mb-56 lg:max-w-md">
+        <div className="flex flex-col mt-10 md:mt-5 max-w-sm lg:mt-0 lg:mx-5 lg:justify-end lg:flex-1 lg:mb-56 lg:max-w-md">
           <h1 className="font-sans text-2xl lg:text-3xl">
             Hi, I'm <span className="font-bold">Jonas Pilloud</span>
           </h1>
@@ -57,7 +57,7 @@ export default function Hero() {
             reprehenderit nemo nam. Eum?
           </p>
         </div>
-        <div className="flex mt-5 mb-10 lg:my-0 lg:mx-2">
+        <div className="flex mt-5 mb-0 lg:my-0 lg:mx-2">
           <ul className="flex flex-col items-start justify-end lg:h-full lg:justify-evenly">
             <NavItem href="#about-me">ABOUT ME</NavItem>
             <NavItem href="#career">CAREER</NavItem>
