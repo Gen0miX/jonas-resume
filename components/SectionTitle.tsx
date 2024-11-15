f13d125720ac2079f5bcca48c179be6ef795d527
Gen0miX <j0naas_14@hotmail.com>
2024-11-15 13:39:56 +0100

    SectionTitle +  AboutMe Anim ok

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState, useRef } from "react";
+import { motion, useInView } from "framer-motion";
 
 interface Props {
   children: ReactNode;
@@ -7,14 +8,68 @@ interface Props {
 }
 
 export default function SectionTitle({ children, className }: Props) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: "all" });
+  const [text, setText] = useState<string[]>([]);
+
+  useEffect(() => {
+    // Convertir le children en tableau de caractères s'il est une string
+    if (typeof children === "string") {
+      setText(children.split(""));
+    }
+  }, [children]);
+
+  // Configuration de l'animation
+  const container = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.05, // Délai entre chaque lettre
+        delayChildren: 0.2,
+      },
+    },
+  };
+
+  const letterVariant = {
+    hidden: { y: 100, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        y: {
+          type: "spring",
+          damping: 10,
+          stiffness: 80,
+        },
+        opacity: {
+          duration: 0.4,
+          ease: "backInOut",
+        },
+      },
+    },
+  };
+
   return (
-    <h1
+    <motion.h1
+      ref={ref}
+      variants={container}
+      initial="hidden"
+      animate={isInView ? "visible" : "hidden"}
       className={clsx(
-        "font-heading text-right text-5xl mb-5 ml-10 sm:text-6xl sm:text-left lg:text-8xl ",
+        "font-heading text-right text-5xl mb-5 ml-10 sm:text-6xl sm:text-left lg:text-8xl",
         className
       )}
     >
-      {children}
-    </h1>
+      {text.map((letter, index) => (
+        <motion.span
+          key={index}
+          variants={letterVariant}
+          style={{ display: "inline-block" }} // Important pour l'animation
+        >
+          {letter === " " ? "\u00A0" : letter}
+        </motion.span>
+      ))}
+    </motion.h1>
   );
 }
--- components/sections/AboutMe.tsx
+++ components/sections/AboutMe.tsx
@@ -1,19 +1,81 @@
 import Image from "next/image";
+import { useRef } from "react";
+import { motion, useInView } from "framer-motion";
 import profilePic from "../../public/images/profile.png";
 import { AiFillGithub, AiFillLinkedin, AiFillMail } from "react-icons/ai";
 import SectionTitle from "../SectionTitle";
 
 export default function AboutMe() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 0.5 });
+
+  const containerVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        staggerChildren: 0.2,
+        delayChildren: 0.3,
+      },
+    },
+  };
+
+  const imgVariants = {
+    hidden: { opacity: 0, y: -100 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        type: "spring",
+        stiffness: 80,
+        damping: 11,
+      },
+    },
+  };
+
+  const txtRVariants = {
+    hidden: { opacity: 0, x: 100 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        stiffness: 80,
+        damping: 11,
+      },
+    },
+  };
+
+  const txtLVariants = {
+    hidden: { opacity: 0, x: -100 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "spring",
+        stiffness: 80,
+        damping: 11,
+      },
+    },
+  };
+
   return (
     <section
+      ref={ref}
       id="about-me"
       className="flex flex-col mt-40 items-center scroll-mt-28 xl:mx-32 2xl:m-60 lg:items-start"
     >
       <SectionTitle className="hidden lg:text-8xl lg:block">
         À PROPOS
       </SectionTitle>
-      <div className="flex flex-col items-center 2xl:mx-auto lg:flex-row">
-        <div className="avatar mb-14 lg:ml-10">
+      <motion.div
+        variants={containerVariants}
+        initial="hidden"
+        animate={isInView ? "visible" : "hidden"}
+        className="flex flex-col items-center 2xl:mx-auto lg:flex-row"
+      >
+        <motion.div variants={imgVariants} className="avatar mb-14 lg:ml-10">
           <div className=" ring-base-content ring-offset-base-content max-w-80 rounded-full ring ring-offset-2 lg:min-w-80 2xl:min-w-96">
             <Image
               src={profilePic}
@@ -23,36 +85,46 @@ export default function AboutMe() {
               className=""
             />
           </div>
-        </div>
+        </motion.div>
 
         <div className="flex flex-col justify-center ml-5 mr-5 md:ml-10 2xl:max-w-[50rem]">
           <SectionTitle className="mt-5 lg:hidden">À PROPOS</SectionTitle>
-          <p className="font-sans text-xl font-medium text-justify lg:text-2xl ">
-            Bonjour ! Je suis Jonas, un développeur junior diplômé en
-            informatique de gestion. J'aime me décrire comme une personne qui
-            s'adapte facilement aux situations, toujours prête à aider les
-            autres et à privilégier une approche de travail claire et organisée.
-            Créer des projets utiles et agréables à utiliser me procure une
-            grande satisfaction. Mon engagement envers l'apprentissage quotidien
-            m'incite à toujours chercher à améliorer mes compétences.
-          </p>
-          <p className="font-sans text-xl font-medium text-justify lg:text-2xl">
-            Actuellement, je suis en quête d'un emploi qui me permettra de
-            mettre en pratique mes connaissances tout en développant de
-            nouvelles aptitudes. Je rêve de rejoindre une équipe qui valorise la
-            collaboration et la créativité, où chacun peut apporter sa touche
-            personnelle.
-          </p>
-          <p className="font-sans text-xl font-medium text-justify lg:text-2xl">
-            En dehors du développement, je suis passionné par le snowboard en
-            hiver et le skateboard en été. J'apprécie aussi me baigner en eau
-            froide tout au long de l'année. C'est une expérience qui me permet
-            de me ressourcer et de clarifier mes pensées. Les jeux vidéo et les
-            jeux de société sont également des activités que j'affectionne, car
-            ils me permettent de tisser des liens tout en gardant mon esprit
-            stratégique aiguisé.
-          </p>
-          <div className="flex justify-end mt-10">
+          <motion.div variants={txtRVariants}>
+            <p className="font-sans text-xl font-medium text-justify lg:text-2xl ">
+              Bonjour ! Je suis Jonas, un développeur junior diplômé en
+              informatique de gestion. J'aime me décrire comme une personne qui
+              s'adapte facilement aux situations, toujours prête à aider les
+              autres et à privilégier une approche de travail claire et
+              organisée. Créer des projets utiles et agréables à utiliser me
+              procure une grande satisfaction. Mon engagement envers
+              l'apprentissage quotidien m'incite à toujours chercher à améliorer
+              mes compétences.
+            </p>
+          </motion.div>
+          <motion.div variants={txtLVariants}>
+            {" "}
+            <p className="font-sans text-xl font-medium text-justify lg:text-2xl">
+              Actuellement, je suis en quête d'un emploi qui me permettra de
+              mettre en pratique mes connaissances tout en développant de
+              nouvelles aptitudes. Je rêve de rejoindre une équipe qui valorise
+              la collaboration et la créativité, où chacun peut apporter sa
+              touche personnelle.
+            </p>
+          </motion.div>
+
+          <motion.div variants={txtRVariants}>
+            <p className="font-sans text-xl font-medium text-justify lg:text-2xl">
+              En dehors du développement, je suis passionné par le snowboard en
+              hiver et le skateboard en été. J'apprécie aussi me baigner en eau
+              froide tout au long de l'année. C'est une expérience qui me permet
+              de me ressourcer et de clarifier mes pensées. Les jeux vidéo et
+              les jeux de société sont également des activités que
+              j'affectionne, car ils me permettent de tisser des liens tout en
+              gardant mon esprit stratégique aiguisé.
+            </p>
+          </motion.div>
+
+          <motion.div variants={imgVariants} className="flex justify-end mt-10">
             <a
               className="mx-1 text-3xl transition-transform duration-300 ease-in hover:-skew-x-6 hover:scale-105 hover:scale-y-125"
               href=""
@@ -71,9 +143,9 @@ export default function AboutMe() {
             >
               <AiFillMail></AiFillMail>
             </a>
-          </div>
+          </motion.div>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
