f3b00b3139be5a327405ae1b18510e531fef0786
Gen0miX <j0naas_14@hotmail.com>
2024-11-17 21:03:28 +0100

    skills anim ok

--- components/sections/Skills.tsx
+++ components/sections/Skills.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useRef, useEffect, useState } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
 import SectionTitle from "../SectionTitle";
 
 interface Props {
@@ -7,74 +8,208 @@ interface Props {
   info?: string;
 }
 
+const textVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      stiffness: 80,
+      damping: 11,
+      delay: 0.8,
+    },
+  },
+};
+
+const divVariants = {
+  hidden: { opacity: 0, y: -100 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: 80,
+      damping: 11,
+      delay: 0.2,
+    },
+  },
+};
+
 function Skill({ children, value }: Props) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      controls.start({ width: `${value}%` });
+    }
+  }, [isInView, controls, value]);
+
   return (
-    <div className="flex items-center w-full ease-in-out hover:text-primary group transition-color">
-      <div className="w-32 mb-2 sm:w-40">
+    <div
+      ref={ref}
+      className="flex items-center w-full ease-in-out hover:text-primary group transition-color"
+    >
+      <motion.div
+        variants={textVariants}
+        initial="hidden"
+        animate={isInView ? "visible" : "hidden"}
+        className="w-32 mb-2 sm:w-40"
+      >
         <p className="font-sans text-base font-medium leading-none transition-all ease-in-out sm:text-lg sm:leading-none lg:text-xl lg:leading-none group-hover:font-bold">
           {children}
         </p>
-      </div>
+      </motion.div>
       <div className="flex items-center flex-1">
         <span className="w-10 mr-1 font-sans text-sm font-bold text-right transition-opacity ease-in-out opacity-0 group-hover:opacity-100">
           {value}%
         </span>
-        <progress
-          className="static flex-1 h-2 transition-colors ease-in-out rounded progress group-hover:progress-primary"
-          value={value}
-          max="100"
-        ></progress>
+        <div className="static flex-1 h-2 transition-colors ease-in-out rounded progress group-hover:progress-primary ">
+          <motion.div
+            className="h-full rounded bg-base-content group-hover:bg-primary"
+            initial={{ width: 0 }}
+            animate={controls}
+            transition={{ duration: 1 }}
+          ></motion.div>
+        </div>
       </div>
     </div>
   );
 }
 function Language({ children, info, value }: Props) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+  const controls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      controls.start({ width: `${value}%` });
+    }
+  }, [isInView, controls, value]);
+
   return (
-    <div className="flex items-center w-full ease-in-out hover:text-primary group transition-color">
-      <div className="flex flex-col mb-1 w-36 sm:w-40">
+    <div
+      ref={ref}
+      className="flex items-center w-full ease-in-out hover:text-primary group transition-color"
+    >
+      <motion.div
+        variants={textVariants}
+        initial="hidden"
+        animate={isInView ? "visible" : "hidden"}
+        className="flex flex-col mb-1 w-36 sm:w-40"
+      >
         <p className="font-sans text-base font-medium leading-none transition-all ease-in-out sm:text-lg sm:leading-none lg:text-xl lg:leading-none group-hover:font-bold">
           {children}
         </p>
         <p className="font-sans text-sm leading-none transition-all ease-in-out sm:text-base sm:leading-none lg:text-lg lg:leading-none">
           {info}
         </p>
-      </div>
-
+      </motion.div>
       <div className="flex items-center flex-1">
         <span className="w-10 mr-1 font-sans text-sm font-bold text-right transition-opacity ease-in-out opacity-0 group-hover:opacity-100">
           {value}%
         </span>
-        <progress
-          className="static flex-1 h-2 transition-colors ease-in-out rounded progress group-hover:progress-primary"
-          value={value}
-          max="100"
-        ></progress>
+        <div className="static flex-1 h-2 transition-colors ease-in-out rounded progress group-hover:progress-primary ">
+          <motion.div
+            className="h-full rounded bg-base-content group-hover:bg-primary"
+            initial={{ width: 0 }}
+            animate={controls}
+            transition={{ duration: 1 }}
+          ></motion.div>
+        </div>
       </div>
     </div>
   );
 }
 
 function CTitle({ children }: Props) {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 1 });
+  const [text, setText] = useState<string[]>([]);
+
+  useEffect(() => {
+    // Convertir le children en tableau de caractères s'il est une string
+    if (typeof children === "string") {
+      setText(children.split(""));
+    }
+  }, [children]);
+
+  // Configuration de l'animation
+  const container = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.05, // Délai entre chaque lettre
+        delayChildren: 0.2,
+      },
+    },
+  };
+
+  const letterVariant = {
+    hidden: { y: 100, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        y: {
+          type: "spring",
+          damping: 10,
+          stiffness: 80,
+        },
+        opacity: {
+          duration: 0.4,
+          ease: "backInOut",
+        },
+      },
+    },
+  };
+
   return (
-    <h2 className="mb-3 font-sans text-2xl font-bold lg:text-4xl">
-      {children}
-    </h2>
+    <motion.h2
+      ref={ref}
+      variants={container}
+      initial="hidden"
+      animate={isInView ? "visible" : "hidden"}
+      className="mb-3 font-sans text-2xl font-bold lg:text-4xl"
+    >
+      {text.map((letter, index) => (
+        <motion.span
+          key={index}
+          variants={letterVariant}
+          style={{ display: "inline-block" }} // Important pour l'animation
+        >
+          {letter === " " ? "\u00A0" : letter}
+        </motion.span>
+      ))}
+    </motion.h2>
   );
 }
 
 export default function Skills() {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true, amount: 1 });
+
   return (
     <section
       id="skills"
       className="mt-40 mb-10 ml-5 mr-5 xl:mx-32 2xl:mx-60 2xl:mb-20 2xl:mt-60 scroll-mt-32"
     >
       <SectionTitle className="ml-5">CAPACITÉS</SectionTitle>
-      <div className="flex flex-col items-center">
+      <motion.div
+        ref={ref}
+        variants={divVariants}
+        initial="hidden"
+        animate={isInView ? "visible" : "hidden"}
+        className="flex flex-col items-center"
+      >
         <p className="font-sans text-base font-medium sm:text-lg lg:text-xl">
           Voici un résumé de mes compétences et aptitudes les plus importantes :
         </p>
         <div className="self-center w-1/4 divider"></div>
-      </div>
+      </motion.div>
       <div className="flex flex-col justify-between md:flex-row">
         <div className="flex flex-col justify-evenly md:grow md:mr-5 xl:mr-10">
           <div className="mb-3 mr-2">
