c5a9b521cd04ed2ed13b6139295b52f030fe6c43
Gen0miX <j0naas_14@hotmail.com>
2024-11-18 19:45:35 +0100

    chang cv name

--- components/cv/CVDownloadButton.tsx
+++ components/cv/CVDownloadButton.tsx
@@ -11,10 +11,10 @@ export default function CVDownloadButton({
   className,
 }: CVDownloadButtonProps) {
   const handleDownload = () => {
-    const pdfUrl = "/images/CV.pdf"; // Chemin vers le fichier PDF dans le dossier public
+    const pdfUrl = "/images/Jonas_Pilloud.pdf"; // Chemin vers le fichier PDF dans le dossier public
     const link = document.createElement("a");
     link.href = pdfUrl;
-    link.download = "CV.pdf";
+    link.download = "Jonas_Pilloud.pdf";
     link.click();
   };
 
